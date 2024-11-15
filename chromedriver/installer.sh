3c2e24d736d831d5d383eff8b05855baccb1a5a4
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-11-15 20:06:24 +0100

    Updated installer.sh

--- chromedriver/installer.sh
+++ chromedriver/installer.sh
@@ -4,12 +4,9 @@ description="This shell script installs the chromedriver version that matches th
 
 chrome_version=$(mdls -name kMDItemVersion /Applications/Google\ Chrome.app | awk '{print $3}' | tr -d '"')
 chrome_major_version=$(echo $chrome_version | cut -d '.' -f 1)
-
-echo "Current Google Chrome Version: {$chrome_version}"
-echo "Chrome Major Version: {$chrome_major_version}"
+echo "Current Google Chrome Version: $chrome_version"
+echo "Chrome Major Version: $chrome_major_version"
 
 sudo npm -g install chromedriver@$chrome_major_version
-
 installation=$(npm list -g chromedriver | grep chromedriver | awk -F '@' '{print $2}')
-
-echo "ChromeDriver Installed: {$installation}"
+echo "ChromeDriver Installed: $installation"
